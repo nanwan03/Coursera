030ce840523a06f28019d8769603b24451b5a606
nanwan03 <wn1842@gmail.com>
2018-07-07 16:28:18 -0700

    commit

--- 机器学习技法/Quiz5/Quiz5.py
+++ 机器学习技法/Quiz5/Quiz5.py
@@ -19,15 +19,18 @@ def loadData(filename):
 def mistake(yhat, y):
     err = np.sum(yhat != y)/len(y)
     return err
+def transform(Y, target):
+    Ytest = Y.copy()
+    pos1 = Y == target
+    pos2 = Y != target
+    Ytest[pos1] = 1
+    Ytest[pos2] = -1
+    return Ytest
 X, Y = loadData('features_train.dat')
 Xtest, Ytest = loadData('features_test.dat')
 
 row, col = X.shape
-Ytemp = Y.copy()
-pos1 = Ytemp == 0
-pos2 = Ytemp != 0
-Ytemp[pos1] = 1
-Ytemp[pos2] = -1
+Ytemp = transform(Y, 0)
 clf = SVC(C=0.01, kernel='linear', shrinking=False)
 clf.fit(X, Ytemp)
 print(np.linalg.norm(clf.coef_))
@@ -36,11 +39,7 @@ Ein = np.zeros((5, ))
 alpha = np.zeros((5, ))
 clf = SVC(C=0.01, kernel = 'poly', degree = 2, gamma = 1, coef0 = 1, shrinking = False)
 for i in range(0, 5):
-    Ytemp = Y.copy()
-    pos1 = Ytemp == i * 2
-    pos2 = Ytemp != i * 2
-    Ytemp[pos1] = 1
-    Ytemp[pos2] = -1
+    Ytemp = transform(Y, i * 2)
     clf.fit(X, Ytemp)
     yhat = clf.predict(X)
     Ein[i] = mistake(yhat, Ytemp)
@@ -50,39 +49,26 @@ print(np.max(alpha))
 
 gamma = np.array([1, 10, 100, 1000, 10000])
 eout = np.zeros((len(gamma),))
-Ytemp = Y.copy()
-pos1 = Ytemp == 0
-pos2 = Ytemp != 0
-Ytemp[pos1] = 1
-Ytemp[pos2] = -1
-Ytesttemp = Ytest.copy()
-pos1 = Ytesttemp == 0
-pos2 = Ytesttemp != 0
-Ytesttemp[pos1] = 1
-Ytesttemp[pos2] = -1
+Ytemp = transform(Y, 0)
+Ytesttemp = transform(Ytest, 0)
 for i in range(len(gamma)):
-    clf = SVC(C=0.1, kernel='rbf', gamma=gamma[i], shrinking=False)
+    clf = SVC(C=0.1, kernel = 'rbf', gamma = gamma[i], shrinking=False)
     clf.fit(X, Ytemp)
     yhat = clf.predict(Xtest)
     eout[i] = mistake(yhat, Ytesttemp)
 print(gamma[np.argmin(eout)])
 
-evali = np.zeros((len(gamma),))
-Ytemp = Y.copy()
-pos1 = Ytemp == 0
-pos2 = Ytemp != 0
-Ytemp[pos1] = 1
-Ytemp[pos2] = -1
-for i in range(len(gamma)):
-    for j in range(5):
-        pos = np.random.permutation(row)
-        Xval = X[pos[0:1000], :]
-        Yval = Ytemp[pos[0:1000]]
-        Xtrain = X[pos[1000:], :]
-        Ytrain = Ytemp[pos[1000:]]
-        clf = SVC(C=0.1, kernel='rbf', gamma=gamma[i], shrinking=False)
-        clf.fit(Xtrain, Ytrain)
-        yhat = clf.predict(Xval)
-        evali[i] += mistake(yhat, Yval)
-    evali[i] /= 5
-print(gamma[np.where(evali == np.min(evali))[0][0]])
+Ytemp = transform(Y, 0)
+evali = 0
+for j in range(100):
+    randops = np.random.permutation(row)
+    Xval = X[randops[0:1000], :]
+    Yval = Ytemp[randops[0:1000]]
+    Xtrain = X[randops[1000:], :]
+    Ytrain = Ytemp[randops[1000:]]
+    clf = SVC(C=0.1, kernel='rbf', gamma=10, shrinking=False)
+    clf.fit(Xtrain, Ytrain)
+    yhat = clf.predict(Xval)
+    evali += mistake(yhat, Yval)
+evali /= 100
+print(evali)
